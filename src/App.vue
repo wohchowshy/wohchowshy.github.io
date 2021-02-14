455b6016b44e35f0762ef3086d602b7a2c6053f2
Yizzy <yizzy@nlplab.cc>
2021-02-14 19:17:39 +0800

    refactor: remove header to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="w-full h-full" :class="[$store.state.isDark? 'darkModeBackground-dark':'darkModeBackground']">
+    <header-bar></header-bar>
     <!-- <div id="nav"> -->
       <!-- <router-link to="/">Home</router-link> | -->
       <!-- <router-link to="/about">About</router-link> -->
@@ -9,6 +10,13 @@
 </template>
 
 <script>
+import HeaderBar from './components/HeaderBar.vue'
+
+export default {
+  components:{
+    HeaderBar
+  }
+}
 
 </script>
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="h-full w-full">
-    <header-bar></header-bar>
+    <!-- <header-bar></header-bar> -->
     <intro-card></intro-card>
   </div>
 </template>
 
 <script>
-import HeaderBar from "@/components/HeaderBar.vue"
+// import HeaderBar from "@/components/HeaderBar.vue"
 import IntroCard from "@/components/IntroCard.vue"
 
 export default {
   components: {
-    HeaderBar, IntroCard
+    IntroCard
   },
 
 }
