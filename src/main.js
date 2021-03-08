cd98ab81740c966dcb98f3cca4c562a115394b6a
Yizzy <yizzy@nlplab.cc>
2021-03-09 01:35:41 +0800

    fix: project.vue Height: full -> auto

--- src/main.js
+++ src/main.js
@@ -6,5 +6,6 @@ import './assets/css/tailwind.css'
 // import 'highlight.js/styles/github.css'
 // import 'highlight.js/styles/dark.css'
 import 'highlight.js/styles/vs2015.css'
+import 'github-markdown-css/github-markdown.css'
 
 createApp(App).use(store).use(router).mount('#app')
