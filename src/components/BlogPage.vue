2f9fa068f653f03b5bdb7a50d473ba871d62f2c1
Yizzy <yizzy@nlplab.cc>
2021-03-13 16:08:14 +0800

    refactor: blog

--- src/components/BlogPage.vue
+++ src/components/BlogPage.vue
@@ -7,34 +7,49 @@
             <div class="date" v-show="content.PublishTime"><i class="far fa-clock"></i>{{ content.PublishTime }}</div>
             <div class="subclass" v-show="content.Subclass">
                 <i class="far fa-folder"></i>
-                <router-link :to="genLink" class="classLink">{{ content.Subclass }}</router-link>
+                <router-link :to="genLink(content.Subclass)" class="classLink">{{ content.Subclass }}</router-link>
             </div>
         </div>
         <div class="image" v-show="content.Image"><img :src="content.Image" alt="oops"/></div>
         <div class="markdown">
             <Marked :content="content.Content"/>
         </div>
+        <div class="sepLine" v-show="content.Title"></div>
         <div class="hashtags">
-            <Hashtags :hashtags="content.Hashtags"/>
+            <i class="fas fa-tags mr-2"></i>
+            <div v-for="(tag, idx) in getCleanHashtags" :key="tag+idx" v-show="tag" class="tags">
+                <router-link :to="genLink(tag)" class="classLink">{{ tag }}</router-link>
+            </div>
+            <!-- <Hashtags :hashtags="content.Hashtags"/> -->
         </div>
     </div>
 </template>
 
 <script>
 import Marked from "@/components/Marked.vue"
-import Hashtags from "@/components/Hashtags.vue"
+// import Hashtags from "@/components/Hashtags.vue"
 
 export default {
     components: {
         Marked,
-        Hashtags
+        // Hashtags
     },
     props: {
         content: Object 
     },
+    methods: {
+        genLink(link) {
+            return '/blog?class='+link;
+        }
+    },
     computed: {
-        genLink() {
-            return '/blog?class='+this.content.Subclass;
+        getCleanHashtags() {
+            let newHashtags = [...this.content.Hashtags]
+            while(newHashtags.length !== 0 && newHashtags[newHashtags.length-1] === ""){
+                newHashtags.pop()
+            }
+            console.log(newHashtags)
+            return newHashtags
         }
     }
     
@@ -60,5 +75,16 @@ export default {
 }
 .classLink:hover {
     @apply underline;
+}
+
+.hashtags {
+    @apply flex mt-4 text-xs text-center;
+}
+.tags {
+    @apply mx-0.5 text-green-600;
+}
+.tags:not(:last-child)::after {
+    content: ",";
+    @apply text-black;
 }
 </style>
